6b4470f2ad2c8a59a237a0e55cab34439ba2205a
Keigo Suzukawa <k.suzukawa@gmail.com>
2015-09-20 11:32:05 +0100

    Stoppable/startable components

--- project.clj
+++ project.clj
@@ -1,18 +1,13 @@
 (defproject the-playground "0.1.0-SNAPSHOT"
 
-  :description "FIXME: write description"
-
-  :url "http://example.com/FIXME"
-
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
   :dependencies [[org.clojure/clojure "1.7.0"]
+                 [com.taoensso/timbre "4.1.1"]
+                 [com.stuartsierra/component "0.2.3"]
                  [ring/ring-core "1.4.0"]
                  [http-kit "2.1.19"]
-                 [bidi "1.20.3"]
-                 [jarohen/embed-nrepl "0.1.2"]
-                 [jarohen/yoyo "0.0.5"]
-                 [jarohen/yoyo.http-kit "0.0.4"]]
+                 [bidi "1.20.3"]]
 
   :main the-playground.core)
--- src/the_playground/core.clj
+++ src/the_playground/core.clj
@@ -1,27 +1,82 @@
 (ns the-playground.core
-  (:require [org.httpkit.server :as http-server]
+  (:require [com.stuartsierra.component :as component]
+            [org.httpkit.server :as http-server]
             [bidi.ring :refer (make-handler)]
             [clojure.tools.nrepl.server :as nrepl-server]
-            [cider.nrepl :refer (cider-nrepl-handler)]))
+            [cider.nrepl :refer (cider-nrepl-handler)]
+            [taoensso.timbre :refer [info]]))
+
 
 (defn api-handler [req]
+  (info "Request to /api")
   {:status  200
    :headers {"Content-Type" "text/html"}
    :body    "Welcome to the API!"})
 
+
 (defn not-found-handler [req]
+  (info "Request to unknown route")
   {:status  404
    :headers {"Content-Type" "text/html"}
    :body    "Not found."})
+
 
 (def routes
  ["/" [["api" :api]
        [true  :not-found]]])
 
+
 (def handler-fns
-  {:api      api-handler
+  {:api       api-handler
    :not-found not-found-handler})
 
+
+(defrecord HTTPServer [port server]
+  component/Lifecycle
+
+  (start [component]
+    (let [handler (make-handler routes handler-fns)
+          http-server (http-server/run-server handler {:port port :join? false})]
+      (info "Started http server on port" port)
+      (assoc component :http-server http-server)))
+
+  (stop [component]
+    (when-let [http-server (:http-server component)]
+      (http-server :timeout 500)
+      (info "Stopped http server")
+      (dissoc component :http-server))))
+
+
+(defn make-http-server
+  [port]
+  (map->HTTPServer {:port port}))
+
+
+(defn make-system
+  [{:keys [port] :as config}]
+  (component/system-map
+    :http-server (make-http-server port)))
+
+
+(def system nil)
+
+
+(defn init []
+  (alter-var-root #'system (constantly (make-system {:port 8080}))))
+
+
+(defn start-system
+  []
+  (alter-var-root #'system component/start))
+
+
+(defn stop-system []
+  (alter-var-root #'system
+                  (fn [s] (when s (component/stop s)))))
+
+
 (defn -main [& args]
-  (nrepl-server/start-server :port 8088 :handler cider-nrepl-handler)
-  (http-server/run-server (make-handler routes handler-fns) {:port 8080}))
+  (let [nrepl-port 8088
+        http-port  8080]
+    (nrepl-server/start-server :port nrepl-port :handler cider-nrepl-handler)
+    (info "Started nREPL server on port" nrepl-port)))
