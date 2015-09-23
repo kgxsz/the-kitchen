cf64a9de519fe1181debf6dbd1b96d35141af3da
Keigo Suzukawa <k.suzukawa@gmail.com>
2015-09-23 21:35:24 +0100

    Use nomad to pick up config

--- project.clj
+++ project.clj
@@ -8,6 +8,7 @@
                  [com.stuartsierra/component "0.2.3"]
                  [org.clojure/tools.namespace "0.2.11"]
                  [jarohen/yoyo "0.0.6-beta2"]
+                 [jarohen/nomad "0.8.0-beta3" :exclusions [org.clojure/clojure]]
                  [ring/ring-core "1.4.0"]
                  [http-kit "2.1.19"]
                  [bidi "1.20.3"]]
--- src/the_playground/core.clj
+++ src/the_playground/core.clj
@@ -1,34 +1,43 @@
 (ns the-playground.core
   (:require [yoyo :as y]
+            [nomad :as n]
             [yoyo.core :as yc]
             [yoyo.system :as ys]
             [org.httpkit.server :refer [run-server]]
             [bidi.ring :refer [make-handler]]
             [cats.core :as c]
+            [clojure.java.io :as io]
             [clojure.tools.nrepl.server :as nrepl-server]
             [cider.nrepl :refer (cider-nrepl-handler)]
             [taoensso.timbre :refer [info]]))
 
 (defn api-handler [req]
   (info "Request to /api")
-  {:status  200
+  {:status 200
    :headers {"Content-Type" "text/html"}
-   :body    "Welcome to the API!"})
+   :body "Welcome to the API!"})
 
 (defn not-found-handler
   [req]
   (info "Request to unknown route")
-  {:status  404
+  {:status 404
    :headers {"Content-Type" "text/html"}
-   :body    "Not found."})
+   :body "Not found."})
 
 (def routes
   ["/" [["api" :api]
-        [true  :not-found]]])
+        [true :not-found]]])
 
 (def handler-fns
-  {:api       api-handler
+  {:api api-handler
    :not-found not-found-handler})
+
+(defn make-Δ-config
+  []
+  (ys/->dep
+    (yc/->component
+      (nomad/read-config
+        (io/file "config.edn")))))
 
 (defn make-Δ-handler
   []
@@ -38,20 +47,21 @@
 
 (defn make-Δ-http-server
   []
-  (c/mlet [handler (ys/ask :handler)]
+  (c/mlet [options (ys/ask :config :http-server-options)
+           handler (ys/ask :handler)]
     (ys/->dep
-      (let [options {:port 8080 :join? false}
-            stop-fn! (run-server handler options)]
-        (info "Starting HTTP server on port 8080")
+      (let [stop-fn! (run-server handler options)]
+        (info "Starting HTTP server on port" (:port options))
         (yc/->component
           options
           (fn []
             (info "Stopping HTTP server")
-            (stop-fn! :timeout 500)))))))
+            (stop-fn! :timeout (:timeout options))))))))
 
 (defn make-system []
-  (ys/make-system #{(ys/named make-Δ-http-server :http-server)
-                    (ys/named make-Δ-handler :handler)}))
+  (ys/make-system #{(ys/named make-Δ-config :config)
+                    (ys/named make-Δ-handler :handler)
+                    (ys/named make-Δ-http-server :http-server)}))
 
 (defn -main []
   (info "Starting nREPL server on port 8088")
