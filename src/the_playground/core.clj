71db911b3bc2ee57696d4fa725aaf02e51090f5a
Keigo Suzukawa <k.suzukawa@gmail.com>
2015-09-24 21:29:12 +0100

    Make config available at handler creation time

--- src/the_playground/core.clj
+++ src/the_playground/core.clj
@@ -11,46 +11,55 @@
             [cider.nrepl :refer (cider-nrepl-handler)]
             [taoensso.timbre :refer [info]]))
 
-(defn api-handler [req]
-  (info "Request to /api")
-  {:status 200
-   :headers {"Content-Type" "text/html"}
-   :body "Welcome to the API!"})
+(defn make-api-handler
+  [config]
+  (fn [req]
+    (info "Request to /api")
+    {:status 200
+     :headers {"Content-Type" "text/html"}
+     :body (str "Welcome to the API! The secret password is: " (:password config))}))
 
-(defn not-found-handler
-  [req]
-  (info "Request to unknown route")
-  {:status 404
-   :headers {"Content-Type" "text/html"}
-   :body "Not found."})
+(defn make-not-found-handler
+  []
+  (fn [req]
+    (info "Request to unknown route")
+    {:status 404
+     :headers {"Content-Type" "text/html"}
+     :body "Not found."}))
 
-(def routes
+(defn make-routes
+  []
   ["/" [["api" :api]
         [true :not-found]]])
-
-(def handler-fns
-  {:api api-handler
-   :not-found not-found-handler})
 
 (defn make-Δ-config
   []
   (ys/->dep
-    (yc/->component
-      (nomad/read-config
-        (io/file "config.edn")))))
+   (yc/->component
+    (nomad/read-config
+     (io/file "config.edn")))))
 
-(defn make-Δ-handler
+(defn make-Δ-routes
   []
   (ys/->dep
-    (yc/->component
-      (make-handler routes handler-fns))))
+   (yc/->component
+     (make-routes))))
+
+(defn make-Δ-handler-fns
+  []
+  (c/mlet [config (ys/ask :config)]
+    (ys/->dep
+      (yc/->component
+        {:api (make-api-handler config)
+         :not-found (make-not-found-handler)}))))
 
 (defn make-Δ-http-server
   []
   (c/mlet [options (ys/ask :config :http-server-options)
-           handler (ys/ask :handler)]
+           routes (ys/ask :routes)
+           handler-fns (ys/ask :handler-fns)]
     (ys/->dep
-      (let [stop-fn! (run-server handler options)]
+      (let [stop-fn! (run-server (make-handler routes handler-fns) options)]
         (info "Starting HTTP server on port" (:port options))
         (yc/->component
           options
@@ -60,7 +69,8 @@
 
 (defn make-system []
   (ys/make-system #{(ys/named make-Δ-config :config)
-                    (ys/named make-Δ-handler :handler)
+                    (ys/named make-Δ-routes :routes)
+                    (ys/named make-Δ-handler-fns :handler-fns)
                     (ys/named make-Δ-http-server :http-server)}))
 
 (defn -main []
