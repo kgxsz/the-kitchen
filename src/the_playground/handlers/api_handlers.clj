73ec879f29552d382df57a3e3dbb93a66577c37c
Keigo Suzukawa <k.suzukawa@gmail.com>
2015-10-25 12:28:56 +0000

    Indentation fix up

--- src/the_playground/handlers/api_handlers.clj
+++ src/the_playground/handlers/api_handlers.clj
@@ -8,39 +8,39 @@
   (-> (fn [req]
         {:status 200
          :body (sc/validate s/UsersResponse
-                 {:users [{:id 123, :name "Bob"}
-                          {:id 321, :name "Jane"}]})})
+                            {:users [{:id 123, :name "Bob"}
+                                     {:id 321, :name "Jane"}]})})
 
       (m/wrap-docs {:summary "Gets a list of users"
-                  :description "Lists all the users"
-                  :tags ["Users"]
-                  :responses {200 {:schema s/UsersResponse
-                                   :description "The list of users"}}})))
+                    :description "Lists all the users"
+                    :tags ["Users"]
+                    :responses {200 {:schema s/UsersResponse
+                                     :description "The list of users"}}})))
 
 (defn make-create-user-handler
   []
   (-> (fn [req]
         {:status 201
          :body (sc/validate s/CreateUserResponse
-                 {:user {:id 456 :name "Alice"}})})
+                            {:user {:id 456 :name "Alice"}})})
 
       (m/wrap-docs {:summary "Creates a user"
-                  :description "Creates a user"
-                  :tags ["Users"]
-                  :responses {201 {:schema s/CreateUserResponse
-                                   :description "The created user"}}})))
+                    :description "Creates a user"
+                    :tags ["Users"]
+                    :responses {201 {:schema s/CreateUserResponse
+                                     :description "The created user"}}})))
 
 (defn make-articles-handler
   []
   (-> (fn [req]
         {:status 200
          :body (sc/validate s/ArticlesResponse
-                 {:articles [{:id 176, :title "Things I like", :text "I like cheese and bread."}
-                             {:id 346, :title "Superconductivity", :text "It's really hard to understand."}]})})
+                            {:articles [{:id 176, :title "Things I like", :text "I like cheese and bread."}
+                                        {:id 346, :title "Superconductivity", :text "It's really hard to understand."}]})})
 
       (m/wrap-docs {:summary "Gets a list of articles"
-                  :description "Lists all the articles"
-                  :tags ["Articles"]
-                  :responses {200 {:schema s/ArticlesResponse
-                                   :description "The list of articles"}}})))
+                    :description "Lists all the articles"
+                    :tags ["Articles"]
+                    :responses {200 {:schema s/ArticlesResponse
+                                     :description "The list of articles"}}})))
 
