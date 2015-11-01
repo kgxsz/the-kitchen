c98c219fb9fd2f7e32d8fbfa0fb50d869990b9ca
Keigo Suzukawa <k.suzukawa@gmail.com>
2015-11-01 17:24:48 +0000

    Update tests with mock db

--- test/the_playground/core_test.clj
+++ test/the_playground/core_test.clj
@@ -21,23 +21,16 @@
 (deftest end-to-end-test
   (yc/with-component (make-Δ-test-system)
     (fn [test-system]
+      (let [users-url (str "http://localhost:" (get-in test-system [:config :http-server-port]) "/api/users")]
 
-      (testing "A The list of users can be explored"
-        (let [users-res @(http/get "http://localhost:8084/api/users")]
-          (is (= 2 (-> users-res :body (parse-string true) :users count)))))
+        (testing "A user can be created"
+          (let [number-of-users-before (-> @(http/get users-url) :body (parse-string true) :users count)
+                create-user-response @(http/post users-url
+                                        {:headers {"Content-Type" "application/json"
+                                                   "Accept" "application/json"}
+                                         :body (generate-string {:name "Peter"})})
+                number-of-users-after (-> @(http/get users-url) :body (parse-string true) :users count)]
 
-      (testing "A user can be created"
-        (let [users-res-initial @(http/get "http://localhost:8084/api/users")
-              create-user-res @(http/post "http://localhost:8084/api/users"
-                                          {:headers {"Content-Type" "application/json"
-                                                     "Accept" "application/json"}
-                                           :body (generate-string {:name "Peter"})})
-              users-res-final @(http/get "http://localhost:8084/api/users")]
-
-          (is (= 2 (-> users-res-initial :body (parse-string true) :users count)))
-          (is (= 201 (:status create-user-res)))
-          (is (= 3 (-> users-res-final :body (parse-string true) :users count)))))
-
-      (testing "A The list of articles can be explored"
-        (let [articless-res @(http/get "http://localhost:8084/api/articles")]
-          (is (= 2 (-> articless-res :body (parse-string true) :articles count))))))))
+            (is (= 201 (:status create-user-response)))
+            (is (= "Peter" (-> create-user-response :body (parse-string true) :user :name)))
+            (is (= (inc number-of-users-before) number-of-users-after))))))))
